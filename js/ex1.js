2211348490714e145d0dcefd506c1171e1732eef
buffalosushi817 <44121873+buffalosushi817@users.noreply.github.com>
2021-03-21 23:47:20 -0700

    completed ex1

--- js/ex1.js
+++ js/ex1.js
@@ -1,29 +1,66 @@
 
-function myFunction() {
-  bodyNodes();
-  document.getElementById("lblInstruction").innerHTML  = 'Results are also in console';
 
-}
+window.addEventListener("load", myLoadEvent);
 
-const bodyNodes = () => {
-  document.getElementById("div-body").childNodes.forEach(
-    node => {
-      console.log(node)
-      document.getElementById("lblMessage").innerHTML  += node.textContent + '<br/>';
+function myLoadEvent() {
+  const houses = [
+    {
+      code: "ST",
+      name: "Stark"
+    },
+    {
+      code: "LA",
+      name: "Lannister"
+    },
+    {
+      code: "BA",
+      name: "Baratheon"
+    },
+    {
+      code: "TA",
+      name: "Targaryen"
     }
-  )
-}
-
-
-function clearValues() {
-  document.getElementById("lblMessage").innerHTML  = '';
-  document.getElementById("lblInstruction").innerHTML  = "Click 'run' program to view results";
+  ];
   
+  var x = document.getElementById("house");
+  for (var i = 0; i < houses.length; i++) { 
+    var option = document.createElement("option");
+    option.text = houses[i].name;
+    option.value = houses[i].code;
+    x.add(option);
+  }
+
+  document.getElementById("house").addEventListener("change", myFunction);
 }
 
+function myFunction() {
+  document.getElementById("characters").innerHTML = "";
 
+  const getCharacters = houseCode => {
+    switch (houseCode) {
+      case "ST":
+        return ["Eddard", "Catelyn", "Robb", "Sansa", "Arya", "Jon Snow"];
+      case "LA":
+        return ["Tywin", "Cersei", "Jaime", "Tyrion"];
+      case "BA":
+        return ["Robert", "Stannis", "Renly"];
+      case "TA":
+        return ["Aerys", "Daenerys", "Viserys"];
+      default:
+        return []; // Empty array
+    }
+  };
 
-
-
-
-
+  let e = document.getElementById("house");
+  myArray = [];
+  myArray = getCharacters(e.value)
+   
+  var ul = document.getElementById("characters");
+  
+  for (var i = 0; i < myArray.length; i++) {
+      var name = myArray[i];
+      var li = document.createElement('li');
+      li.appendChild(document.createTextNode(name));
+      ul.appendChild(li);
+  }
+}
